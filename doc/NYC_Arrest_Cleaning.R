da7b76094c8e5aad04774ddeea26d3a1df113b40
Alex DeKinder <ad3540@columbia.edu>
2019-10-08 15:18:05 -0400

    Update NYC_Arrest_Cleaning.R

--- doc/NYC_Arrest_Cleaning.R
+++ doc/NYC_Arrest_Cleaning.R
@@ -14,19 +14,19 @@ cleaned_NYPD_2019 <- NYPD_Arrest_Data_Year_to_Date_ %>%
   mutate(ARREST_DATE = lubridate::mdy(ARREST_DATE)) %>%
   group_by(ARREST_DATE,ARREST_PRECINCT) %>%
   tally() %>%
-  rename(Count = n)
+  rename(Count = n) 
 
 
-cleaned_NYPD_2013_2018 <- NYPD_Arrests_Data_Historic_ %>%
+cleaned_NYPD_2018 <- NYPD_Arrests_Data_Historic_ %>%
   mutate(ARREST_DATE = lubridate::mdy(ARREST_DATE)) %>%
-  filter(ARREST_DATE > "2012-12-31" & ARREST_DATE < "2019-01-01") %>%
+  filter(ARREST_DATE > "2018-09-02" & ARREST_DATE < "2019-01-01") %>%
   group_by(ARREST_DATE,ARREST_PRECINCT) %>%
   tally() %>%
   rename(Count = n)
 
 #Combining historic and 2019 data
 
-cleaned_NYPD_Arrests <- rbind(cleaned_NYPD_2019,cleaned_NYPD_2013_2018)
+cleaned_NYPD_Arrests <- rbind(cleaned_NYPD_2019,cleaned_NYPD_2018)
 
 write.csv(cleaned_NYPD_Arrests,file = "~/output/cleaned_NYPD_Arrests.csv")
 
