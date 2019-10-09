89d286eee7388108918ec5c04226e9b0b5ccaaab
Kangkang Zhang <kangkang.zseven@gmail.com>
2019-10-08 22:13:39 -0400

    updates

--- app/server.R
+++ app/server.R
@@ -1,168 +1,249 @@
 library(shiny)
-library(choroplethr)
-library(choroplethrZip)
-library(dplyr)
+library(shinythemes)
+library(tidyverse)
 library(leaflet)
-library(maps)
-library(rgdal)
+library(lubridate)
+library(scales)
+library(sf)
+library(tigris)
+library(nycgeo)
 
-## Define Manhattan's neighborhood
-man.nbhd=c("all neighborhoods", "Central Harlem", 
-           "Chelsea and Clinton",
-           "East Harlem", 
-           "Gramercy Park and Murray Hill",
-           "Greenwich Village and Soho", 
-           "Lower Manhattan",
-           "Lower East Side", 
-           "Upper East Side", 
-           "Upper West Side",
-           "Inwood and Washington Heights")
-zip.nbhd=as.list(1:length(man.nbhd))
-zip.nbhd[[1]]=as.character(c(10026, 10027, 10030, 10037, 10039))
-zip.nbhd[[2]]=as.character(c(10001, 10011, 10018, 10019, 10020))
-zip.nbhd[[3]]=as.character(c(10036, 10029, 10035))
-zip.nbhd[[4]]=as.character(c(10010, 10016, 10017, 10022))
-zip.nbhd[[5]]=as.character(c(10012, 10013, 10014))
-zip.nbhd[[6]]=as.character(c(10004, 10005, 10006, 10007, 10038, 10280))
-zip.nbhd[[7]]=as.character(c(10002, 10003, 10009))
-zip.nbhd[[8]]=as.character(c(10021, 10028, 10044, 10065, 10075, 10128))
-zip.nbhd[[9]]=as.character(c(10023, 10024, 10025))
-zip.nbhd[[10]]=as.character(c(10031, 10032, 10033, 10034, 10040))
-
-## Load housing data
-load("../output/count.RData")
-load("../output/mh2009use.RData")
-
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+function(input, output, session) {
   
-  ## Neighborhood name
-  output$text = renderText({"Selected:"})
-  output$text1 = renderText({
-      paste("{ ", man.nbhd[as.numeric(input$nbhd)+1], " }")
+  # cleaned_NYPD_df <- read_csv("./output/cleaned_NYPD_Arrests.csv")
+  # save(cleaned_NYPD_df, file = "./temp_app/cleaned_NYPD_Arrests.Rdata")
+  #load("./temp_app/DSNY_Graffiti_Tracking.RData")
+  #load("./temp_app/DSNY_Graffiti_Tracking.RData")
+  load("DSNY_Graffiti_Tracking.RData")
+  load("cleaned_NYPD_Arrests.Rdata")
+  
+  nhyc_cd_data <- cd_sf
+  
+  gra_df = gra_df %>% 
+    janitor::clean_names() %>% 
+    filter(!is.na(latitude) &  !is.na(longitude) & !is.na(city_council_district)) %>%
+    mutate(cd_id = str_extract(community_board, "[[:digit:]]+"),
+           borough_id = case_when(
+             borough == "MANHATTAN" ~ "1",
+             borough == "BRONX" ~ "2",
+             borough == "BROOKLYN" ~ "3",
+             borough == "QUEENS" ~ "4",
+             borough == "STATEN ISLAND" ~ "5"),
+           borough_cd_id = str_c(borough_id, cd_id))
+  
+  temp_df =  gra_df %>% 
+    filter(status == "Closed") %>% 
+    group_by(borough_cd_id, status) %>% 
+    summarise(close_count = n()) %>%
+    left_join(gra_df %>% 
+                group_by(borough_cd_id) %>% 
+                summarise(ttl_count = n()), by = "borough_cd_id") %>%  
+    mutate(close_rate = close_count/ttl_count) %>%
+    ungroup() 
+  
+  map_data <- geo_join(nhyc_cd_data, temp_df, "borough_cd_id",  "borough_cd_id", how = "inner") %>% 
+    mutate(ttl_count = as.numeric(ttl_count), close_rate = as.numeric(close_rate))
+  
+  #############tab 1###################
+  choices <- c("Select All", "Cleaning crew dispatched", 
+               "Cannot identify property owner",
+               "Graffiti is intentional.", 
+               "Graffiti Reported", 
+               "Mail returned / wrong address",
+               "Notice of Intent form sent", 
+               "Property Research Required",
+               "Public Property (Non-City)",
+               "Site downloaded for cleaning",
+               "Site to be cleaned.")
+  
+  ######### Map Data ########
+  
+  #Graffiti <- read.csv("./temp_app/DSNY_Graffiti_Tracking.csv")
+  load("Graffiti.RData")
+  Graffiti_clean <- Graffiti %>% 
+    filter(is.na(LATITUDE) == FALSE & is.na(LONGITUDE) == FALSE) %>% 
+    filter(is.na(COMMUNITY_BOARD) == FALSE & is.na(CREATED_DATE) == FALSE & is.na(STATUS) == FALSE &
+             is.na(RESOLUTION_ACTION) == FALSE & is.na(BOROUGH) == FALSE & is.na(POLICE_PRECINCT) == FALSE)
+  
+  Graffiti_grouped <- Graffiti_clean
+  Graffiti_grouped$RESOLUTION_ACTION <- as.character(Graffiti_grouped$RESOLUTION_ACTION)
+  Graffiti_grouped$RESOLUTION_ACTION[Graffiti_grouped$RESOLUTION_ACTION == "Cleaning crew dispatched.  Owner refused."] <- "Cleaning crew dispatched"
+  Graffiti_grouped$RESOLUTION_ACTION[Graffiti_grouped$RESOLUTION_ACTION == "Cleaning crew dispatched.  Property cleaned."] <- "Cleaning crew dispatched"
+  Graffiti_grouped$RESOLUTION_ACTION[Graffiti_grouped$RESOLUTION_ACTION == "Cleaning crew dispatched. No graffiti on property."] <- "Cleaning crew dispatched"
+  Graffiti_grouped$RESOLUTION_ACTION[Graffiti_grouped$RESOLUTION_ACTION == "Cleaning crew dispatched.  Cannot locate property."] <- "Cleaning crew dispatched"
+  Graffiti_grouped$RESOLUTION_ACTION[Graffiti_grouped$RESOLUTION_ACTION == "Further research is necessary to identify an owner for the property."] <- "Cannot identify property owner"
+  Graffiti_grouped$RESOLUTION_ACTION[Graffiti_grouped$RESOLUTION_ACTION == "Notice of Intent to Clean and Forever graffiti free form sent"] <- "Notice of Intent form sent"
+  
+  observe({
+    if ("Select All" %in% input$variable) {
+      # choose all the choices _except_ "Select All"
+      selected_choices <- setdiff(choices, "Select All")
+      updateSelectInput(session, "variable", selected = selected_choices)
+    }
+  })
+  observe({
+    if(input$empty != 0){
+      updateSelectInput(session, "variable", selected = "")
+    }
+  })
+  df <- reactive({
+    Graffiti_grouped %>%
+      filter(as.Date(CREATED_DATE, "%m/%d/%Y") >= as.Date(input$daterange[1], "%m/%d/%Y") & 
+               as.Date(CREATED_DATE, "%m/%d/%Y") <= as.Date(input$daterange[2], "%m/%d/%Y")) %>%
+      filter(as.character(STATUS) == input$status)%>%
+      filter(RESOLUTION_ACTION %in% input$variable) 
+    
   })
   
-  ## Panel 1: summary plots of time trends, 
-  ##          unit price and full price of sales. 
+  
+  output$map <- renderLeaflet({
+    m <- leaflet(data = df()) %>%
+      addTiles() %>%
+      addCircleMarkers(lng = ~LONGITUDE, 
+                       lat = ~LATITUDE,
+                       popup = paste("Police precinct:", df()$POLICE_PRECINCT, "<br>",
+                                     "Status:", df()$STATUS, "<br>",
+                                     "Closed Date:", df()$CLOSED_DATE), radius = 5, 
+                       color = ~ ifelse(df()$STATUS == 'Open', 'red', 'blue'), 
+                       clusterOptions = markerClusterOptions())
+    m
+  })
+  #############tab 2 ###############
+  
+  output$map2 <- renderLeaflet({
+    
+    if(input$selectb == "count"){
+      bins <- c(0, 200, 400, 600, 800, 1000, 1400, 1800)
+      pal <- colorBin("Reds", domain = map_data$ttl_count, bins = bins)
+      popup1 = paste0('<strong>Count: </strong><br>', map_data$ttl_count,
+                      '<br><strong>Close Rate: </strong><br>', round(map_data$close_rate,2),
+                      '<br><strong>Name: </strong><br>', map_data$cd_name)
+      
+      map_data %>%
+        st_transform(., "+init=epsg:4326") %>%
+        leaflet() %>%
+        addTiles() %>%
+        addPolygons(popup = popup1,
+                    layerId=~borough_cd_id,
+                    #label = ~ttl_count,
+                    fillColor = ~pal(ttl_count),
+                    color = 'grey', 
+                    fillOpacity = .6,
+                    weight = 1,
+                    dashArray = "3") %>% 
+        addProviderTiles("CartoDB.Positron") %>% 
+        addLegend(pal = pal, values = ~bins, opacity = 0.6, title = "Number of Complaints",
+                  position = "bottomright")
+    } else{
+      bins2 <- c(0.3, 0.5, 0.7, 0.9, 1)
+      pal2 <- colorBin("Blues", domain = map_data$close_rate, bins = bins2)
+      popup2 = paste0('<strong>Count: </strong><br>', map_data$ttl_count,
+                      '<br><strong>Close Rate: </strong><br>', round(map_data$close_rate,2), 
+                      '<br><strong>Name: </strong><br>', map_data$cd_name)
+      
+      map_data %>%
+        st_transform(., "+init=epsg:4326") %>%
+        leaflet() %>%
+        addTiles() %>%
+        addPolygons(popup = popup2,
+                    layerId=~borough_cd_id,
+                    #label = ~ttl_count,
+                    fillColor = ~pal2(close_rate),
+                    color = 'grey', 
+                    fillOpacity = .6,
+                    weight = 1,
+                    dashArray = "3") %>% 
+        addProviderTiles("CartoDB.Positron") %>% 
+        addLegend(pal = pal2, values = ~bins2, opacity = 0.6, title = "Close rate",
+                  position = "bottomright")
+    }
+  })
+  
+  observe({
+    #whenever map item is clicked, becomes event
+    event <- input$map2_shape_click
+    if (is.null(event))
+      return()
+    
+    output$month_trend <- renderPlot({
+      #returns null if no borough selected
+      if (is.na(event$id)) {
+        return(NULL)
+      }
+      
+      gra_df %>% mutate(shown_date = format.Date(as.Date(created_date, format = "%m/%d/%Y"), "%b %y"),
+                        sort_date = format(as.Date(created_date, format = "%m/%d/%Y"), "%Y%m")) %>%
+        mutate(cd_id = as.character(as.numeric(str_extract(city_council_district, "[[:digit:]]+")))) %>%
+        filter(borough_cd_id == "101") %>%
+        group_by(shown_date, sort_date) %>%
+        dplyr::summarize(ttl = n()) %>%
+        ungroup() %>%
+        arrange(sort_date) %>%
+        mutate(shown_date = factor(shown_date, levels = shown_date)) %>%
+        ggplot(aes(x=shown_date, y = ttl)) +
+        geom_bar(stat="identity", na.rm = TRUE, color="lightblue", fill = "lightblue") +
+        theme_bw() +
+        theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+        labs(x = "Month", y = "Number of complaints", title = "Number of complaints monthly trend")
+    })
+  })
+  
+  observe({
+    #whenever map item is clicked, becomes event
+    event <- input$map2_shape_click
+    if (is.null(event))
+      return()
+    
+    output$police_plot <- renderPlot({
+      #returns null if no borough selected
+      if (is.na(event$id)) {
+        return(NULL)
+      }
+      
+      cleaned_NYPD_df %>% 
+        mutate(shown_date = format.Date(ARREST_DATE, "%b %y"),
+               sort_date = format.Date(ARREST_DATE, "%Y%m")) %>%
+        filter(borough_cd_id == as.numeric(event$id)) %>%
+        group_by(shown_date, sort_date) %>%
+        dplyr::summarize(ttl = sum(Arrest.Count)) %>%
+        ungroup() %>%
+        arrange(sort_date) %>%
+        mutate(shown_date = factor(shown_date, levels = shown_date)) %>%
+        ggplot(aes(x=shown_date, y = ttl)) +
+        geom_bar(stat="identity", na.rm = TRUE, color="orange", fill = "orange") +
+        theme_bw() +
+        theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+        labs(x = "Month", y = "Number of arrests", title = "Number of arrests monthly trend")
+    })
+  })
+  
+  ##############tab 3###############
   
   output$distPlot <- renderPlot({
+    # generate bins based on input$bins from ui.R
+    x    <- fait
+    hful[, 2]
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
     
-    ## First filter data for selected neighborhood
-    mh2009.sel=mh2009.use
-    if(input$nbhd>0){
-      mh2009.sel=mh2009.use%>%
-                  filter(region %in% zip.nbhd[[as.numeric(input$nbhd)]])
-    }
-    
-    ## Monthly counts
-    month.v=as.vector(table(mh2009.sel$sale.month))
-    
-    ## Price: unit (per sq. ft.) and full
-    type.price=data.frame(bldg.type=c("10", "13", "25", "28"))
-    type.price.sel=mh2009.sel%>%
-                group_by(bldg.type)%>%
-                summarise(
-                  price.mean=mean(sale.price, na.rm=T),
-                  price.median=median(sale.price, na.rm=T),
-                  unit.mean=mean(unit.price, na.rm=T),
-                  unit.median=median(unit.price, na.rm=T),
-                  sale.n=n()
-                )
-    type.price=left_join(type.price, type.price.sel, by="bldg.type")
-    
-    ## Making the plots
-    layout(matrix(c(1,1,1,1,2,2,3,3,2,2,3,3), 3, 4, byrow=T))
-    par(cex.axis=1.3, cex.lab=1.5, 
-        font.axis=2, font.lab=2, col.axis="dark gray", bty="n")
-    
-    ### Sales monthly counts
-    plot(1:12, month.v, xlab="Months", ylab="Total sales", 
-         type="b", pch=21, col="black", bg="red", 
-         cex=2, lwd=2, ylim=c(0, max(month.v,na.rm=T)*1.05))
-    
-    ### Price per square foot
-    plot(c(0, max(type.price[,c(4,5)], na.rm=T)), 
-         c(0,5), 
-         xlab="Price per square foot", ylab="", 
-         bty="l", type="n")
-    text(rep(0, 4), 1:4+0.5, paste(c("coops", "condos", "luxury hotels", "comm. condos"), 
-                                  type.price$sale.n, sep=": "), adj=0, cex=1.5)
-    points(type.price$unit.mean, 1:nrow(type.price), pch=16, col=2, cex=2)
-    points(type.price$unit.median, 1:nrow(type.price),  pch=16, col=4, cex=2)
-    segments(type.price$unit.mean, 1:nrow(type.price), 
-              type.price$unit.median, 1:nrow(type.price),
-             lwd=2)    
-    
-    ### full price
-    plot(c(0, max(type.price[,-1], na.rm=T)), 
-         c(0,5), 
-         xlab="Sales Price", ylab="", 
-         bty="l", type="n")
-    text(rep(0, 4), 1:4+0.5, paste(c("coops", "condos", "luxury hotels", "comm. condos"), 
-                                   type.price$sale.n, sep=": "), adj=0, cex=1.5)
-    points(type.price$price.mean, 1:nrow(type.price), pch=16, col=2, cex=2)
-    points(type.price$price.median, 1:nrow(type.price),  pch=16, col=4, cex=2)
-    segments(type.price$price.mean, 1:nrow(type.price), 
-             type.price$price.median, 1:nrow(type.price),
-             lwd=2)    
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
   })
   
-  ## Panel 2: map of sales distribution
-  output$distPlot1 <- renderPlot({
-    count.df.sel=count.df
-    if(input$nbhd>0){
-      count.df.sel=count.df%>%
-        filter(region %in% zip.nbhd[[as.numeric(input$nbhd)]])
-    }
-    # make the map for selected neighhoods
-    
-    zip_choropleth(count.df.sel,
-                   title       = "2009 Manhattan housing sales",
-                   legend      = "Number of sales",
-                   county_zoom = 36061)
+  output$ggplot1 <- renderPlot({
+    ggplot(data = gra_df) +
+      geom_point(mapping = aes(x = longitude, y = latitude, color = resolution_action))
   })
   
-  ## Panel 3: leaflet
-  output$map <- renderLeaflet({
-    count.df.sel=count.df
-    if(input$nbhd>0){
-      count.df.sel=count.df%>%
-        filter(region %in% zip.nbhd[[as.numeric(input$nbhd)]])
+  output$freq <- renderPlot({
+    if ( input$type == "Mapping" ) {
+      ggplot(data = gra_df) +
+        geom_point(mapping = aes(x = longitude, y = latitude, color = resolution_action)) +
+        theme_bw()
+    } else if ( input$type == "Resolution") {
+      freq(gra_df$resolution_action)
+    } else if ( input$type == "Borough") {
+      freq(gra_df$borough)
     }
-    
-    # From https://data.cityofnewyork.us/Business/Zip-Code-Boundaries/i8iw-xf4u/data
-    NYCzipcodes <- readOGR("../data/ZIP_CODE_040114.shp",
-                           #layer = "ZIP_CODE", 
-                           verbose = FALSE)
-    
-    selZip <- subset(NYCzipcodes, NYCzipcodes$ZIPCODE %in% count.df.sel$region)
-    
-    # ----- Transform to EPSG 4326 - WGS84 (required)
-    subdat<-spTransform(selZip, CRS("+init=epsg:4326"))
-    
-    # ----- save the data slot
-    subdat_data=subdat@data[,c("ZIPCODE", "POPULATION")]
-    subdat.rownames=rownames(subdat_data)
-    subdat_data=
-      subdat_data%>%left_join(count.df, by=c("ZIPCODE" = "region"))
-    rownames(subdat_data)=subdat.rownames
-    
-    # ----- to write to geojson we need a SpatialPolygonsDataFrame
-    subdat<-SpatialPolygonsDataFrame(subdat, data=subdat_data)
-    
-    # ----- set uo color pallette https://rstudio.github.io/leaflet/colors.html
-    # Create a continuous palette function
-    pal <- colorNumeric(
-      palette = "Blues",
-      domain = subdat$POPULATION
-    )
-    
-    leaflet(subdat) %>%
-      addTiles()%>%
-      addPolygons(
-        stroke = T, weight=1,
-        fillOpacity = 0.6,
-        color = ~pal(POPULATION)
-      )
   })
-})
+  
+}
--- app/ui.r
+++ app/ui.r
@@ -1,42 +1,125 @@
 library(shiny)
+library(shinythemes)
+library(tidyverse)
 library(leaflet)
+library(DataExplorer)
+library(funModeling)
+library(Hmisc)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("2009 Manhattan Housing Sales"),
-  
-  # Sidebar with a selector input for neighborhood
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("nbhd", label = h5("Choose a Manhattan Neighborhood"), 
-                         choices = list("all neighborhoods"=0,
-                                        "Central Harlem"=1, 
-                                        "Chelsea and Clinton"=2,
-                                        "East Harlem"=3, 
-                                        "Gramercy Park and Murray Hill"=4,
-                                        "Greenwich Village and Soho"=5, 
-                                        "Lower Manhattan"=6,
-                                        "Lower East Side"=7, 
-                                        "Upper East Side"=8, 
-                                        "Upper West Side"=9,
-                                        "Inwood and Washington Heights"=10), 
-                         selected = 0)
-      #sliderInput("p.range", label=h3("Price Range (in thousands of dollars)"),
-      #            min = 0, max = 20000, value = c(200, 10000))
-    ),
-    # Show two panels
-    mainPanel(
-      #h4(textOutput("text")),
-      h3(code(textOutput("text1"))),
-      tabsetPanel(
-        # Panel 1 has three summary plots of sales. 
-        tabPanel("Sales summary", plotOutput("distPlot")), 
-        # Panel 2 has a map display of sales' distribution
-        tabPanel("Sales map", plotOutput("distPlot1"))),
-      leafletOutput("map", width = "80%", height = "400px")
-    )
- )
-))
-
+navbarPage(windowTitle = "NYC Graffiti",
+           title = div(img(src="NYC.jpg",height = 30,
+                           width = 50), "Graffiti"), 
+           id="gra", theme = shinytheme("journal"), #themeSelector(),
+           
+           ########1st Panel##############
+           
+           tabPanel("Spot Map", icon = icon("map-pin"),
+                    tags$style(".outer {position: fixed; top: 41px; left: 0; right: 0; bottom: 0; overflow: hidden; padding: 0}"),
+                    div(class = "outer",
+                        
+                        tags$head(
+                          # Include our custom CSS
+                          includeCSS("styles.css"),
+                          includeScript("gomap.js")
+                        ),
+                        
+                        leafletOutput("map", width = "100%", height = "100%"),
+                        
+                        absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                                      draggable = TRUE, top = 70, left = "auto", right = 20, bottom = "auto",
+                                      width = 330, height = "auto",
+                                      
+                                      
+                                      dateRangeInput("daterange", h3("Created Date range:"),
+                                                     start  = "2018-09-03",
+                                                     end    = "2019-09-02",
+                                                     min    = "2018-09-03",
+                                                     max    = "2019-09-02",
+                                                     format = "mm/dd/yy",
+                                                     separator = " - "),
+                                      
+                                      selectInput("variable", h3("Resolution Action:"),
+                                                  c("Cleaning crew dispatched" = "Cleaning crew dispatched", 
+                                                    "Cannot identify property owner" = "Cannot identify property owner",
+                                                    "Graffiti is intentional." = "Graffiti is intentional.", 
+                                                    "Graffiti Reported" = "Graffiti Reported", 
+                                                    "Mail returned / wrong address" = "Mail returned / wrong address",
+                                                    "Notice of Intent form sent" = "Notice of Intent form sent", 
+                                                    "Property Research Required" = "Property Research Required",
+                                                    "Public Property (Non-City)" ="Public Property (Non-City)",
+                                                    "Site downloaded for cleaning" = "Site downloaded for cleaning",
+                                                    "Site to be cleaned." = "Site to be cleaned.",
+                                                    "Select All"), multiple=TRUE, selected = "Select All"),
+                                      verbatimTextOutput("selected"),
+                                      actionLink("empty", "empty choices"),
+                                      
+                                      checkboxGroupInput("status", label = h3("Status:"),
+                                                         choices = c("Open" = "Open",
+                                                                     "Closed" = "Closed", 
+                                                                     "Pending" = "Pending"),
+                                                         selected = "Open"))
+                    )
+           ),
+           
+           ########2nd Panel##############
+           
+           tabPanel("Heat Map", icon = icon("map-signs"),
+                    tags$style(".outer {position: fixed; top: 41px; left: 0; right: 0; bottom: 0; overflow: hidden; padding: 0}"),
+                    div(class = "outer",
+                        
+                        tags$head(
+                          # Include our custom CSS
+                          includeCSS("styles.css"),
+                          includeScript("gomap.js")
+                        ),
+                        
+                        leafletOutput("map2", width = "100%", height = "100%"),
+                        
+                        absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                                      draggable = TRUE, top = 150, left = 0, right = 40, bottom = "auto",
+                                      width = 400, height = "auto",
+                                      br(),
+                                      radioButtons("selectb", label = strong("Layers"),
+                                                   choices = list("Count" = "count", "Close Rate" = "close_rate"), 
+                                                   selected = "count"),
+                                      
+                                      p(strong("Click on a community district to see the time trend of number of graffiti complaints.")),
+                                      
+                                      plotOutput("month_trend", height = 280),
+                                      plotOutput("police_plot", height = 280)
+                                      
+                        )
+                    )
+                    
+           ),
+           
+           ########3rd Panel##############
+           
+           tabPanel("Frequencies", icon = icon("chart-bar"),
+                    fluidRow(column(12,
+                                    h1("Graffiti Frequencies"),
+                                    p("The distribution of reported grafitti incidents vary by borough. There are more incidents in certain boroughs than other ones- this can be both due to the culture amongst different neighborhoods in the borough, but also can be due to other factors such as socioeconomic conditions. It is also important to note that just because there was a grafitti incident reported does not mean that there was actual grafitti. The third option, mapping, will show the different types of incidents that occured."),
+                                    br(),
+                                    h4("Types of charts"),
+                                    p("Select the buttons on the left to choose the type of chart to display. Resolution shows the different types of outcomes after reporting the grafitti incident. Borough shows the frequency of grafitti reports in the different boroughs. Resolution shows the different types of outcomes by color based on the latitude and longitude"))),
+                    hr(),
+                    fluidRow(sidebarPanel(width = 3,
+                                          h4("Resolution Action vs Borough"),
+                                          helpText("Chose whether you would like to see the frequency of graffiti in terms of borough, resolution, or mapping."),
+                                          radioButtons("type", NULL,
+                                                       c("Resolution" = "Resolution",
+                                                         "Borough" = "Borough",
+                                                         "Mapping" = "Mapping"))),
+                             mainPanel(plotOutput("freq", height = 500)))
+           ),
+           
+           
+           ########4th Panel##############
+           
+           tabPanel("About", icon = icon("info"),
+                    fluidRow(),
+                    fluidRow(),
+                    hr()
+           )
+           
+)
